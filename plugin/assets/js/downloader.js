724cc2f96372b509a51f33a56c52d3ae49a24613
wokrgili <134441980+wokrgili@users.noreply.github.com>
2024-11-28 18:29:09 -0500

    Let's See

--- plugin/assets/js/downloader.js
+++ plugin/assets/js/downloader.js
@@ -3,11 +3,11 @@
 function startpreview(box) {
     let volume = 1;
     if (window.gamesettings) {
-        volume = (window.gamesettings.mastervolume / 60) * (window.gamesettings.musicvolume / 60);
+        volume = (window.gamesettings.mastervolume/100) * (window.gamesettings.musicvolume/100);
         volume = Math.min(1, Math.max(0, volume));
     }
     let audios = document.getElementsByTagName("audio");
-    for (let i = 0; i < audios.length; ++i)
+    for (let i=0; i<audios.length; ++i)
         if (audios[i].softstop)
             audios[i].softstop();
     let a = document.createElement("audio");
@@ -18,21 +18,21 @@ function startpreview(box) {
     a.volume = 0;
     a.play();
     document.body.appendChild(a);
-    let fadeIn = setInterval(function () {
+    let fadeIn = setInterval(function(){
         if (a.volume < volume)
-            a.volume = Math.min(volume, a.volume + 0.05 * volume);
+            a.volume = Math.min(volume, a.volume + 0.05*volume);
         else
             clearInterval(fadeIn);
     }, 30);
-    let fadeOut = setInterval(function () {
+    let fadeOut = setInterval(function(){
         if (a.currentTime > 9.3) // assume it's 10s long
-            a.volume = Math.max(0, a.volume - 0.05 * volume);
+            a.volume = Math.max(0, a.volume - 0.05*volume);
         if (a.volume == 0)
             clearInterval(fadeOut);
     }, 30);
-    a.softstop = function () {
-        let fadeOut = setInterval(function () {
-            a.volume = Math.max(0, a.volume - 0.05 * volume);
+    a.softstop = function() {
+        let fadeOut = setInterval(function(){
+            a.volume = Math.max(0, a.volume - 0.05*volume);
             if (a.volume == 0) {
                 clearInterval(fadeOut);
                 a.remove();
@@ -40,13 +40,21 @@ function startpreview(box) {
         }, 10);
     }
 }
+
+function log_to_server(message) {
+    let url = "http://api.osugame.online/log/?msg=" + message;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.send();
+}
+
 function startdownload(box) {
     startpreview(box);
-    if (box.downloading) { 
-        return;
-    }
-    let url = "https://catboy.best/d/" + box.sid + "?n-0";
-    box.downloading = true;
+	if (box.downloading) {
+		return;
+	}
+	let url = "https://catboy.best/d/" + box.sid;  //https://txy1.sayobot.cn/beatmaps/download/mini/
+	box.downloading = true;
     box.classList.add("downloading");
     let xhr = new XMLHttpRequest();
     xhr.responseType = 'arraybuffer';
@@ -66,19 +74,19 @@ function startdownload(box) {
     bar.max = 1;
     bar.value = 0;
     // async part
-    xhr.onload = function () {
+    xhr.onload = function() {
         box.oszblob = new Blob([xhr.response]);
         bar.className = "finished";
         box.classList.remove("downloading");
-//        log_to_server("got " + box.sid + " in " + (new Date().getTime() - (box.download_starttime || 0)));
+        log_to_server("got " + box.sid + " in " + (new Date().getTime() - (box.download_starttime || 0)));
     }
-    xhr.onprogress = function (e) {
-        bar.value = e.loaded / e.total;
+    xhr.onprogress = function(e) {
+		bar.value = e.loaded / e.total;
     }
-    xhr.onerror = function () {
-        console.error("Download failed");
+    xhr.onerror = function() {
+    	console.error("download failed");
         alert("Beatmap download failed. Please retry later.")
-        box.downloading = false;
+		box.downloading = false;
         box.classList.remove("downloading");
         log_to_server("fail " + box.sid);
     }
